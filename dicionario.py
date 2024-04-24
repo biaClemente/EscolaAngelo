22b3f76c58f507beb447be38af7efeec5dffc148
beatriz clemente <166627017+biaClemente@users.noreply.github.com>
2024-04-24 08:10:47 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,8 @@
+primeiroDicionario = {}
+
+primeiroDicionario['nome'] = "Edson"
+primeiroDicionario['tipo'] = "professor"
+primeiroDicionario["numero"] = 9
+print(primeiroDicionario)
+print(primeiroDicionario["tipo"]) 
+
